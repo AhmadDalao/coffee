87fb8fef6ec462f993bcf6907c0cbf933f8ea674
shadow8evil@gmail.com <shadow8evil@gmail.com>
2019-05-12 01:45:03 +0300

    coffee order app

---
+++ app/src/main/java/com/example/android/coffeeorder/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.coffeeorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    /*
+    this application display order form to order coffee.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+/*
+this method will display the text when the order button is clicked.
+ */
+    public void makeOrder(View view) {
+        TextView textView =(TextView)findViewById(R.id.quantity_text);
+        textView.setText(String.valueOf("Hello world by ahmad"));
+    }
+
+
+
+
+}
+
