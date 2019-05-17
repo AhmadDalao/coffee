93ebd58d2c2bddc8e7c96ecd8e205a24f7d6c98a
shadow8evil@gmail.com <shadow8evil@gmail.com>
2019-05-17 22:12:15 +0300

    final changes program is done

--- app/src/main/java/com/example/android/coffeeorder/MainActivity.java
+++ app/src/main/java/com/example/android/coffeeorder/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     int coffeeQuantity = 1;
 
     /*
-    this application display order form to order coffee.
+    this application display order form to order coffee. and send the order through the email app
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     } // end of onCreate
 
+    /*
+    this method will decrease the coffeeQuantity when the button subt is clicked on
+    also it will make sure to show toast message to the user if they are trying to order less than 1 cup
+    the coffeeQuantity is then passed to the displayQuantity() method to display the change there
+     */
     public void SubtractCoffee(View view) {
         if (coffeeQuantity <= 1) {
             Toast.makeText(this, "you cant have less than 1 cup of coffee", Toast.LENGTH_LONG).show();
@@ -35,7 +40,11 @@ public class MainActivity extends AppCompatActivity {
         displayQuantity(coffeeQuantity);
     }
 
-
+    /*
+       this method will increase  the coffeeQuantity when the button add is clicked on
+       also it will make sure to show toast message to the user if they are trying to order more than 100 cup
+       the coffeeQuantity is then passed to the displayQuantity() method to display the change there
+        */
     public void AddCoffee(View view) {
         if (coffeeQuantity >= 100) {
             Toast.makeText(this, "you cant have more than 100 cups of coffee!!", Toast.LENGTH_SHORT).show();
@@ -44,84 +53,118 @@ public class MainActivity extends AppCompatActivity {
         displayQuantity(coffeeQuantity);
     }
 
+    /*
+    this method calculates the coffee price
 
+    @param number takes the coffeeQuantity and do the math for it
+
+    @param hasChocolate takes a boolean variable to calculate the price when its checked and increase
+    the price by 3 for each coffee cup with chocolate
+
+    @param hasCream takes a boolean variable to calculate the price when its checked and increase
+    the price by 2 for each coffee cup with cream
+
+    @return	  it will return the price back
+
+     */
     public int calculatePrice(int number, boolean hasChocolate, boolean hasCream) {
         int price = 5;
         if (hasChocolate && hasCream) {
             // 5 =  5 * 5 = 25
             price = (number * price);
             price = price + (3 + 2) * number;
-            return price;
         } else if (hasChocolate) {
             price = (number * price);
             price = price + (3 * number);
-            return price;
 
         } else if (hasCream) {
             price = (number * price);
             price = price + (2 * number);
-            return price;
+        } else
+            price = (number * price);
 
-        }
-        price = (number * price);
         return price;
     }
 
+    /*
+    this method display the coffee quantity when its decreasing or increasing
+    @param number will take the coffeeQuantity and display it on the screen
+     */
     public void displayQuantity(int number) {
         TextView textView = findViewById(R.id.quantity_text);
         textView.setText(String.valueOf(number));
     }
 
-
-    // ToDo change it later right it i'm not using it at all
+    /*
+    this method takes the details passed to it from makeOrder and display it inside price_text field
+    @param TakeDetail will show the details on the screen
+     */
     public void displayDetail(String TakeDetail) {
         TextView textView = findViewById(R.id.price_text);
         textView.setText(TakeDetail);
 
     }
 
-    public String displayMessage(String message, int price, boolean hasChocolate, boolean hasCream) {
+    /*
+    this method will prepare the order summary  and return and string contains all the required information
 
-        String theDetail = message;
+    @param nameFromUser is the name we pass from the editText from makeOrder method
+
+    @param price we pass from makeOrder calculated price for sure
+
+    @param hasChocolate is the variable we pass from makeOrder method too , it will show true next to "With chocolate"
+     if the checkbox been clicked on
+
+    @param hasCream is the variable we pass from makeOrder method too , it will show true next to "With cream "
+    if the checkbox been clicked on
+
+    @return theDetail this String variable contains all the required information for the order summary and
+    it will be return once the method been called over
+
+     */
+    public String displayMessage(String nameFromUser, int price, boolean hasChocolate, boolean hasCream) {
+        String theDetail = nameFromUser;
         theDetail = "Name: " + theDetail + "\n";
         theDetail = theDetail + "with chocolate:  " + hasChocolate + "\n";
         theDetail = theDetail + "with cream: " + hasCream + "\n";
         theDetail = theDetail + "Total is $" + price + "\n";
         theDetail = theDetail + "Thank you !!";
-
         return theDetail;
     }
 
+    /*
+    this method will display the order summary and price alongside and will send the order through email
+    once the button Order clicked on
+     */
     public void makeOrder(View view) {
-
+        // taking the name from the user
         EditText myname = findViewById(R.id.edit_text);
-        String tempHolder = String.valueOf(myname.getText());
-
+        // assigning the name from user to String variable so we can use it later on
+        String NameHolder = String.valueOf(myname.getText());
+        // taking object from both checkboxes available
         CheckBox chocolate = findViewById(R.id.chocolate_checkbox);
         CheckBox cream = findViewById(R.id.cream_checkbox);
-
+        // making boolean variable to pass it on later and to check if the checkbox been clicked on
         boolean hasCream = cream.isChecked();
         boolean hasChocolate = chocolate.isChecked();
-
+        // making price variable which will be passed later on to displayMessage() method
+        //i need  assign  calculatePrice() method and pass the following the coffeeQuantity and both the boolean variables
         int price = calculatePrice(coffeeQuantity, hasChocolate, hasCream);
-
-        String detail = displayMessage(tempHolder, price, hasChocolate, hasCream);
-
-
+        // assigning the displayMessage() method to String detail and it will rerun string contains the orderSummary
+        String detail = displayMessage(NameHolder, price, hasChocolate, hasCream);
+        // passing the String detail to be printed on the screen containing all the information : name , price , if the user wants
+        //chocolate or cream it will all be shown on the screen
+        displayDetail(detail);
+        // to send the order through the email when makeOrder is clicked on
         final String[] addresses = {"shadow8evil@gmail.com"};
-
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_EMAIL, addresses);
-        intent.putExtra(Intent.EXTRA_SUBJECT, detail);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Your order \n" + detail);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
-
-
-        displayDetail(detail);
     }
-
 
 } // end of class
 
