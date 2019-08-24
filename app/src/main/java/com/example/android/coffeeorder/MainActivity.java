6092b4f5f874748abb1bbe36568b742c52136a0d
shadow8evil@gmail.com <shadow8evil@gmail.com>
2019-08-24 11:42:03 +0300

    made some changes and i like it

--- app/src/main/java/com/example/android/coffeeorder/MainActivity.java
+++ app/src/main/java/com/example/android/coffeeorder/MainActivity.java
@@ -24,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView textView = (TextView) findViewById(R.id.quantity_text);
+        textView.setText("1");
 
     } // end of onCreate
 
